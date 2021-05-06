1cf436a34e39d35207576b14a9866a87d8b1a600
arif <you@example.com>
2021-05-06 11:39:06 +0530

    foodweb

---
+++ src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import classes from './footer.module.css'
+
+const Footer = () => {
+   
+return(
+    <div className={classes.div}>
+        This site is made by Arif .
+        All copyRights reserved
+    </div>
+)
+
+}
+
+export default Footer;
